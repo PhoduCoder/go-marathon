be78f9c8698a4d540fa31a05c431fa333dee3f6a
Gaurav <gauravprashar17@gmail.com>
2025-09-30 19:31:07 -0400

    Max in a Slice

--- cmd/slices/main.go
+++ cmd/slices/main.go
@@ -10,11 +10,17 @@ func main() {
 	num_list := []int{1, 4, 5, 3, 6, 9, 110, 43, 17, 2}
 	fmt.Printf("The initial slice of number is %v\n", num_list)
 
+	new_list := []int{1, 2, 3, 4, -1, -4, 7, 10, 8, 9}
+
 	rev := slices.Reverse(num_list)
 
 	fmt.Printf("The returned reversed slice is %v\n", rev)
 
-	fmt.Printf("The maximum number in the slice is %d\n", slices.Maximum(num_list))
+	fmt.Printf("The maximum number in original slice is %d\n", slices.Max(num_list))
+
+	fmt.Printf("The maximum number in original slice via idiomatic Go is %d\n", slices.IdiomaticMax(new_list))
+
+	fmt.Printf("The maximum number in the slice is %d\n", slices.Maximum(new_list))
 
 	fmt.Printf("Printing the slice %v & reverse %v again\n", num_list, rev) //Note that the slice is sorted now because of inplace sorting
 }
--- internal/slices/slices.go
+++ internal/slices/slices.go
@@ -13,6 +13,27 @@ func Reverse(x []int) []int {
 	return rev
 }
 
+func Max(x []int) int {
+	max := x[0]
+	for i, _ := range x {
+		if x[i] > max {
+			max = x[i]
+		}
+	}
+	return max
+}
+
+func IdiomaticMax(x []int) int {
+	max := x[0]
+
+	for _, v := range x {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
 func Maximum(x []int) int {
 	sort.Ints(x) //Performs an ascending order inplace sort
 	return (x[len(x)-1])
