be62a593e74bd514fe48ea50a6d270bc11bb4d58
Gaurav <gauravprashar17@gmail.com>
2025-09-30 09:10:04 -0400

    Updated the for loop notes

--- cmd/slices/main.go
+++ cmd/slices/main.go
@@ -7,10 +7,14 @@ import (
 )
 
 func main() {
-	num_list := []int{1, 4, 5, 3, 6, 9, 10, 43, 17, 2}
+	num_list := []int{1, 4, 5, 3, 6, 9, 110, 43, 17, 2}
 	fmt.Printf("The initial slice of number is %v\n", num_list)
 
 	rev := slices.Reverse(num_list)
 
 	fmt.Printf("The returned reversed slice is %v\n", rev)
+
+	fmt.Printf("The maximum number in the slice is %d\n", slices.Maximum(num_list))
+
+	fmt.Printf("Printing the slice %v & reverse %v again\n", num_list, rev) //Note that the slice is sorted now because of inplace sorting
 }
--- internal/slices/slices.go
+++ internal/slices/slices.go
@@ -1,10 +1,20 @@
 package slices
 
+import (
+	"sort"
+)
+
 func Reverse(x []int) []int {
 	rev := make([]int, len(x))
 
-	for i, j := 0, len(x)-1; i < len(x); i, j = i+1, j-1 {
+	for i, j := 0, len(x)-1; i < len(x); i, j = i+1, j-1 { //note that you could NOT do i++ or j--
 		rev[i] = x[j]
 	}
 	return rev
+}
+
+func Maximum(x []int) int {
+	sort.Ints(x) //Performs an ascending order inplace sort
+	return (x[len(x)-1])
+
 }
