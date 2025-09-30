ce5071812046aef02ed61b1a44e4147289837b66
Gaurav <gauravprashar17@gmail.com>
2025-09-30 19:38:55 -0400

    Added-test-cases-Idiomatic-Max

--- internal/slices/slices_test.go
+++ internal/slices/slices_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestSlices(t *testing.T) {
+func TestRev(t *testing.T) {
 	got := Reverse([]int{1, 2, 3, 4, 5})
 
 	want := []int{5, 4, 3, 2, 1}
@@ -16,6 +16,16 @@ func TestSlices(t *testing.T) {
 
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("Reverse() = %q; want := %q", got, want)
+	}
+
+}
+
+func TestIdiomMax(t *testing.T) {
+	got := IdiomaticMax([]int{-3, 4, 1, -5})
+
+	want := 4
+	if got != want {
+		t.Fatalf("IdiomaticMax() = %d, want = %d", got, want)
 	}
 
 }
