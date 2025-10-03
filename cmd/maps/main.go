594e0e539661ff928417d7e4b08531196709099f
Gaurav <gauravprashar17@gmail.com>
2025-10-03 08:28:01 -0400

    Additional code for maps

--- cmd/maps/main.go
+++ cmd/maps/main.go
@@ -10,4 +10,7 @@ func main() {
 	users := maps.GetUsers()
 	fmt.Printf("The users are %v map\n", users)
 	fmt.Printf("The first user is %s\n", users[1])
+	users[5] = "Swapnil"
+	fmt.Println("After modification users map", users)
+
 }
