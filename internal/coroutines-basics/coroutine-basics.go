16bbfaab491a1c1648ef4ae919e28f1c96e9ae44
Gaurav <gauravprashar17@gmail.com>
2025-10-23 18:56:23 -0400

    Added coroutine and synchronize

---
+++ internal/coroutines-basics/coroutine-basics.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	fmt.Println("Starting the main go coroutine")
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	go func() {
+		//defer wg.Done()
+		fmt.Println("=================Starting Anonymous coroutine=============")
+	}() //Forking from the main coroutine
+
+	wg.Wait() //Join point
+
+	iterations := make([]int, 5)
+	for i, _ := range iterations {
+		fmt.Println(i)
+		fmt.Println("Calling after describing the anonymous coroutine")
+	}
+
+}
