f6011465e9d9392d3aa4866abf4e01ea88de9ce6
Gaurav <gauravprashar17@gmail.com>
2026-03-11 23:51:35 -0400

    Explaining basics of funcmap and using functions in templates

---
+++ course-apis/net/http/templates/func-map-basics.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"text/template"
+)
+
+var fm = template.FuncMap{
+	"upper": strings.ToUpper,
+	"lower": strings.ToLower,
+}
+
+func main() {
+
+	// The below doesn't work
+
+	// tpl := template.Must(template.New("mytemp").Parse("This is the sample text {{ upper . }}"))
+	// tpl = tpl.Funcs(fm)
+
+	tpl := template.Must(template.New("").Funcs(fm).Parse("This is the sample text in upper case {{ upper . }} and this is in lower case {{ lower .}} \n "))
+	tpl.Execute(os.Stdout, "Gaurav")
+
+}
