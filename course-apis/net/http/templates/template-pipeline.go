3068736a1ac07acd6928ef19f8f2dd29fca50377
Gaurav <gauravprashar17@gmail.com>
2026-03-23 23:27:33 -0400

    template pipeline

---
+++ course-apis/net/http/templates/template-pipeline.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"text/template"
+	"time"
+)
+
+var tpl *template.Template
+
+var fm = template.FuncMap{
+	"dateOnly": timeformat,
+	"kitchen":  tf_kitchen,
+	"upper":    strings.ToUpper,
+}
+
+func main() {
+
+	tpl := template.Must(template.New("").Funcs(fm).ParseGlob("files/*"))
+
+	tpl.ExecuteTemplate(os.Stdout, "neuf.gohtml", time.Now())
+}
+
+func timeformat(t time.Time) string {
+	return t.Format(time.DateOnly)
+}
+
+func tf_kitchen(t time.Time) string {
+	return t.Format(time.Kitchen)
+}
